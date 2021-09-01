ab5cb994accdf79a25a80af7ce8c4d3f9171f347
Florian Obersteiner <f.obersteiner@posteo.de>
2021-09-01 09:06:02 +0200

    sanity checks

--- tzlocal/tz_windows_test.go
+++ tzlocal/tz_windows_test.go
@@ -2,27 +2,61 @@ package tzlocal
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
+	"time"
 )
+
+func TestMappings(t *testing.T) {
+	// sanity check: can we find all IANA names from WinTZtoIANA in IANAtoWinTZ?
+	for _, v := range WinTZtoIANA {
+		if _, ok := IANAtoWinTZ[v]; !ok {
+			t.Errorf("could not find IANA name %v in IANAtoWinTZ", v)
+		}
+	}
+	// sanity checks: can we find all IANA names from IANAtoWinTZ in WinTZtoIANA?
+	// can we successfully call time.LoadLocation(tzname) for all given IANA names?
+	for k, v := range IANAtoWinTZ {
+		if _, ok := WinTZtoIANA[v]; !ok {
+			t.Errorf("could not find Win tz name %v in WinTZtoIANA", v)
+		}
+		if _, err := time.LoadLocation(k); err != nil {
+			t.Errorf("time.LoadLocation failed for IANA tz name %v", k)
+		}
+	}
+}
 
 func TestLocalTZ(t *testing.T) {
 	s, err := localTZfromTzutil()
-	assert.NoError(t, err)
-	assert.NotEmpty(t, s)
+	if err != nil {
+		t.Errorf("got unexpected error %v", err)
+	}
+	if s == "" {
+		t.Error("got unexpected empty result with no error")
+	}
 
 	s, err = localTZfromReg()
-	assert.NoError(t, err)
-	assert.NotEmpty(t, s)
+	if err != nil {
+		t.Errorf("got unexpected error %v", err)
+	}
+	if s == "" {
+		t.Error("got unexpected empty result with no error")
+	}
 
 	s, err = LocalTZ()
-	assert.NoError(t, err)
-	assert.NotEmpty(t, s)
+	if err != nil {
+		t.Errorf("got unexpected error %v", err)
+	}
+	if s == "" {
+		t.Error("got unexpected empty result with no error")
+	}
 
 	tmp := WinTZtoIANA
 	WinTZtoIANA = map[string]string{}
 	s, err = LocalTZ()
-	assert.Error(t, err)
-	assert.Empty(t, s)
+	if err == nil {
+		t.Error("expected error but got nil")
+	}
+	if s != "" {
+		t.Errorf("expected empty result but got %v", s)
+	}
 	WinTZtoIANA = tmp
 }
